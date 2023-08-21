b6954cf538ee68a1929947816330113c4bb3dc3a
itzik <itsikzigi@gmail.com>
2023-08-21 16:37:24 +0300

    Signed-off-by: itzik <itsikzigi@gmail.com>

--- server-project/users/routes/usersRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { addNewUser, loginRes } = require("../userController");
-
-router.get("/");
-router.post("/sign-up", addNewUser);
-router.post("/log-in", loginRes);
-
-module.exports = router;
